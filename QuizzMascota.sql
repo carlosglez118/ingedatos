fce3e30b657a0d98445d414109f072d8108822f3
Cristianrb04 <92269473+Cristianrb04@users.noreply.github.com>
2024-09-18 08:33:15 -0500

    Add files via upload

--- QuizzMascota.sql
+++ QuizzMascota.sql
@@ -1,7 +1,7 @@
 /*Primer Punto*/
 CREATE DATABASE Mascotas;
 /*Segundo y tercer Punto*/
-USE DATABASE Mascotas;
+USE Mascotas;
 
 CREATE TABLE Mascota(
 	idMascota INT PRIMARY KEY,
@@ -51,3 +51,30 @@ CREATE TABLE Mascota_vacuna(
 ALTER TABLE Producto ADD COLUMN cantidad INT NOT NULL;
 ALTER TABLE Mascota CHANGE COLUMN cantidad cantidadMascota INT;
 ALTER TABLE Mascota_vacuna RENAME TO detalle_vacuna;
+
+use mascotas;
+select *from cliente;
+insert into mascota values (1, 'bola', 'mujer','cruzado', 4);
+insert into mascota values (4, '', '','', '');
+insert into mascota values (2, 'Pluto', 'masculino','criollo', 2), (3, 'nini', 'mujer','mestizo', 1);
+insert into producto values (1, 'jabon', 'dove', '10900','1203', 3);
+insert into vacuna values (1, 'AstraZeneca', '10ml', 'moquillo'), (2, 'Pfizer', '15ml', 'rabia');
+insert into vacuna values (3, 'AstraZeneca', '5ml', 'Pentavelante');
+
+insert into cliente values(1203, 'Cristian', 'Ruiz', 'Calle 19 4-88',3321739886, '3');
+insert into cliente values(4983, 'Carlos', 'Gonzalez', 'Calle 21',3525932010, '1');
+
+update cliente set telefono='3321739886' where cedulaCliente=1203;
+update cliente set telefono='3525932010' where cedulaCliente=4983;
+alter table cliente change column telefono telefono varchar(15);
+
+insert into detalle_vacuna values(1, 3, 'moquillo'), (2, 1, 'rabia');
+
+select *from mascota;
+select *from vacuna;
+select *from detalle_vacuna;
+describe producto;
+describe cliente;
+describe vacuna;
+describe mascota;
+describe detalle_vacuna;
